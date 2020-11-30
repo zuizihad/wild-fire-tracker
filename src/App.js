eaa021551abc35ddb82a053d132ac7e0a5873564
Zihadul Islam <ahmedxihad@gmail.com>
2020-12-01 00:19:15 +0600

    location info box and navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Map from "./components/Map";
 import Loader from "./components/Loader";
-
+import Header from "./components/Header";
 function App() {
   const [eventData, setEventData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -19,7 +19,12 @@ function App() {
     fetchEvents();
   }, []);
 
-  return <div>{!loading ? <Map eventData={eventData} /> : <Loader />}</div>;
+  return (
+    <div>
+      <Header />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import LocationMarker from "./LocationMarker";
+import LocationInfoBox from "./LocationInfoBox";
+
 const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo] = useState(null);
+
   const markers = eventData.map((event) => {
     if (event.categories[0].id === 8) {
       return (
         <LocationMarker
           lat={event.geometries[0].coordinates[1]}
           lng={event.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: event.id, title: event.title })}
         />
       );
     }
@@ -22,6 +28,7 @@ const Map = ({ eventData, center, zoom }) => {
       >
         {markers}
       </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   );
 };
